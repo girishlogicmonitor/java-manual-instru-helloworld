46dbe4eaa3e95aa318a87fe1a0c7b1bc5c8e04d0
Girish Mahajan <girish.mahajan@logicmonitor.com>
2022-06-29 18:08:11 +0530

    Initial checkin

--- src/main/java/com/yourcompany/lmoteldemo/Helloworld.java
+++ src/main/java/com/yourcompany/lmoteldemo/Helloworld.java
@@ -1,16 +1,12 @@
 package com.yourcompany.lmoteldemo;
 
-import com.logicmonitor.resource.LMResourceDetector;
-import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.api.trace.StatusCode;
-import io.opentelemetry.api.trace.Tracer;
-import io.opentelemetry.context.Scope;
+import io.opentelemetry.api.common.AttributesBuilder;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 
 import java.util.concurrent.TimeUnit;
 
@@ -19,15 +15,27 @@ public class Helloworld {
     private static final String SERVICE_NAME = "yourcompany-primary-service";
 
     static {
-        // initialise application start scope sdkTracerProvider
-        Resource serviceResource = LMResourceDetector.detect();
-
+        /*
         // field passed by _JAVA_OPTIONS or JAVA_OPTS or export command to env
         System.setProperty("OTEL_SERVICE_NAME", SERVICE_NAME);
-        System.setProperty("OTEL_RESOURCE_ATTRIBUTES", "host.name=cusmersupportservice_dc02,service.name=cusmersupportservice,service.namespace=,ip=192.168.13.02,datacenter=p01-gcp.yourcompany,foo=bar"/*do custom works?*/);
+        System.setProperty("OTEL_RESOURCE_ATTRIBUTES", "host.name=cusmersupportservice_dc02,service.name=cusmersupportservice,service.namespace=,ip=192.168.13.02,datacenter=p01-gcp.yourcompany,foo=bar");
 
         // your working identy token with logic monitor platform having tracing enabled
         System.setProperty("LOGICMONITOR_BEARER_TOKEN", "<your-company-token>");
+
+        // initialise application start scope sdkTracerProvider
+        // Resource serviceResource = LMResourceDetector.detect();
+*/
+        
+        //Create Resource
+        AttributesBuilder attrBuilders = Attributes.builder()
+                .put(ResourceAttributes.SERVICE_NAME, SERVICE_NAME)
+                .put(ResourceAttributes.SERVICE_NAMESPACE, "us-west-1")
+                .put(ResourceAttributes.HOST_NAME, "dc2.us-west-1.yourcompany.com");
+
+        //Resource serviceResource = LMResourceDetector.detect();
+        Resource serviceResource = Resource
+                .create(attrBuilders.build());
 
 
         //Create Span Exporter either an http or an GRPC
