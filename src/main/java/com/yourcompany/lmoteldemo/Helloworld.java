ab423974e473231908263d0859ece8713f99ff13
Girish Mahajan <girish.mahajan@logicmonitor.com>
2022-06-30 10:33:53 +0530

    Initial checkin

--- src/main/java/com/yourcompany/lmoteldemo/Helloworld.java
+++ src/main/java/com/yourcompany/lmoteldemo/Helloworld.java
@@ -15,22 +15,22 @@ public class Helloworld {
     private static final String SERVICE_NAME = "yourcompany-primary-service";
 
     static {
-        /*
+
         // field passed by _JAVA_OPTIONS or JAVA_OPTS or export command to env
         System.setProperty("OTEL_SERVICE_NAME", SERVICE_NAME);
         System.setProperty("OTEL_RESOURCE_ATTRIBUTES", "host.name=cusmersupportservice_dc02,service.name=cusmersupportservice,service.namespace=,ip=192.168.13.02,datacenter=p01-gcp.yourcompany,foo=bar");
 
         // your working identy token with logic monitor platform having tracing enabled
-        System.setProperty("LOGICMONITOR_BEARER_TOKEN", "<your-company-token>");
+        //System.setProperty("LOGICMONITOR_BEARER_TOKEN", "<your-company-token>");
 
         // initialise application start scope sdkTracerProvider
         // Resource serviceResource = LMResourceDetector.detect();
-*/
+
 
         //Create Resource
         AttributesBuilder attrBuilders = Attributes.builder()
                 .put(ResourceAttributes.SERVICE_NAME, SERVICE_NAME)
-                .put(ResourceAttributes.SERVICE_NAMESPACE, "us-west-1")
+                .put(ResourceAttributes.SERVICE_NAMESPACE, "yourcompanyproduct")
                 .put(ResourceAttributes.HOST_NAME, "dc2.us-west-1.yourcompany.com");
 
         //Resource serviceResource = LMResourceDetector.detect();
@@ -40,8 +40,9 @@ public class Helloworld {
 
         //Create Span Exporter either an http or an GRPC
         OtlpGrpcSpanExporter spanExporter = OtlpGrpcSpanExporter.builder()
-                //.setEndpoint("https://qauattraces01.logicmonitor.com/rest")
-                .setEndpoint("http://localhost:55680")
+                //.setEndpoint("https://<LOGICMONITOR_ACCOUNT_NAME>.logicmonitor.com/rest/api")
+                //.addHeader("Authorization", "Bearer <your company token>")
+                .setEndpoint("http://localhost:4317")
                 .build();
 
         //Create SdkTracerProvider so GlobalOpenTelemetry can understand
@@ -53,7 +54,10 @@ public class Helloworld {
     }
 
     public static void main(String[] args) {
-        YourCompanyOperation operation = new YourCompanyOperation();
-        operation.performCompanyRootOperation();
+        Integer limit= 100000;  // send as many traces you want to understand further deeper
+        for(int i = 0; i <=limit; ++i) {
+            YourCompanyOperation operation = new YourCompanyOperation();
+            operation.performCompanyRootOperation();
+        }
     }
 }
--- src/main/java/com/yourcompany/lmoteldemo/YourCompanyOperation.java
+++ src/main/java/com/yourcompany/lmoteldemo/YourCompanyOperation.java
@@ -33,7 +33,6 @@ public class YourCompanyOperation {
         }
     }
 
-
     /*
     *  CHILD OPERATION 1
     * */
